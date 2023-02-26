76a7808b0dc331e6e1e863857a803890b290ccd5
Yana-Kiselova <116965886+Yana-Kiselova@users.noreply.github.com>
2023-02-26 17:24:53 +0000

    Deploying to gh-pages from @ Yana-Kiselova/icecream@b17716f3d8162a7ea87d29f2c59f3cf46dac66d2 🚀

---
+++ index.2a142218.js
@@ -0,0 +1,2 @@
+$(".slide-reviews").slick({slidesToShow:1,slidesToScroll:1,dots:!0,arrows:!1,asNavFor:".slide-customer",autoplay:!0,infinite:!0}),$(".slide-customer").slick({slidesToShow:1,slidesToScroll:1,asNavFor:".slide-reviews",dots:!0,focusOnSelect:!0,arrows:!1});
+//# sourceMappingURL=index.2a142218.js.map
